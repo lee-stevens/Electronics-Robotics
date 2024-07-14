c2c97353c899053907fb99e354a851c35559a2c5
Lee Stevens <Leepeterstevens@gmail.com>
2024-07-15 00:06:55 +0100

    Trying to write some custom ADC and LED blinking

--- Projects/project_a.py
+++ Projects/project_a.py
@@ -1,16 +1,34 @@
 import RPi.GPIO as GPIO
 import time
+from libraries.ADCDevice import ADCDevice, PCF8591, ADS7830
 
+adc = ADCDevice() # Define an ADCDevice class object
 ledPin = 11    # define ledPin
 
 def setup():
+  setupLeds()
+  setupAdc()
+
+def setupLeds():
   GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering i.e., GPIO 17 is pin 11
   GPIO.setup(ledPin, GPIO.OUT)   # set the ledPin to OUTPUT mode
   GPIO.output(ledPin, GPIO.LOW)  # make the ledPin's output LOW level 
   print ('using pin%d'%ledPin)
 
+
+def setupAdc():
+  global adc
+  if(adc.detectI2C(0x48)): # Detect the pcf8591.
+    adc = PCF8591()
+  elif(adc.detectI2C(0x4b)): # Detect the ads7830
+    adc = ADS7830()
+
 def mainLoop():
   while True:
+    loopLeds()
+    loopAdc()
+
+def loopLeds():
     GPIO.output(ledPin, GPIO.HIGH)  # make ledPin output HIGH level to turn on led
     print ('led turned on >>>')     # print information on terminal
     time.sleep(1)                   # Wait for 1 second
@@ -18,8 +36,15 @@ def mainLoop():
     print ('led turned off <<<')
     time.sleep(1)                   # Wait for 1 second
 
+def loopAdc():
+  value = adc.analogRead(0)    # read the ADC value of channel 0
+  voltage = value / 255.0 * 3.3  # calculate the voltage value
+  print ('ADC Value : %d, Voltage : %.2f'%(value,voltage))
+  time.sleep(0.1)
+
 def destroy():
   GPIO.cleanup()                      # Release all GPIO
+  adc.close()
 
 if __name__ == '__main__':
   print ('Starting Project, please use ctrl+c to end at any point\n')
